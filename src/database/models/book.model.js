77984c8f62c155dd97e9d6c3c244556c4ff82dae
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-14 10:19:00 +0200

    :rocket: Setup book model

---
+++ src/database/models/book.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validators = require('../validators');
+
+const schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, 'User id is required!'],
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  ratings: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      grade: {
+        type: Number,
+        required: true,
+        validate: {
+          validator: (value) => validators.minmax(value, 0, 5),
+          message: _ => 'Grade must be between 0 and 5 included!',
+        }
+      }
+    }
+  ],
+  averageRating: {
+    type: Number,
+  }
+});
+
+const Book = mongoose.model('Book', schema);
+module.exports = Book;

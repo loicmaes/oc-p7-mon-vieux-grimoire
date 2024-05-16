9574d8aa83160c303028a4285a6b4bd11c000ffb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-16 10:45:46 +0200

    :hammer: Update validators methods

--- src/database/validators.js
+++ src/database/validators.js
@@ -1,11 +1,15 @@
 function isEmail (value) {
-  return /^[\w-.]+@([\w-.]\.)[\w-]{2,}$/.test(value);
+  return /^[\w-.]+@([\w-.]+\.)[\w-]{2,}$/.test(value);
 }
 function isStrongPassword (value) {
   return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(value);
+}
+function minmax(value, min, max, strict = false) {
+  return strict ? min < value && value < max : min <= value && value <= max;
 }
 
 module.exports = {
   isEmail,
   isStrongPassword,
+  minmax,
 }

2ee1b0d4995404e77b3e2813210dee62d6de5e52
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-16 10:49:22 +0200

    :memo: Add comments to auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 const {register, login} = require("../functions/auth.function");
 module.exports = (app) => {
+  // ACTION: check the email usage, create and store a user and create an authentication token.
   app.post('/auth/signin', async (req, res) => {
     const { email, password } = req.body;
     await register(res, email, password);
   });
-
+  // ACTION: verify credentials and create an authentication token.
   app.post('/auth/login', async (req, res) => {
     const { email, password } = req.body;
     await login(res, email, password);

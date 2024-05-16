f364ef7cf980069b17569e1bbfe31dd2fb4b9223
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-16 10:46:06 +0200

    :rocket: Add new books routes

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,11 +1,46 @@
 const Book = require('../database/models/book.model');
+const authRequired = require('../middlewares/auth.middleware');
+const fileInterceptor = require('../middlewares/file.middleware');
 
 module.exports = (app) => {
-  app.get('/books', (req, res) => {
-    Book.find({})
-        .then(data => {
-          res.json(data);
-        })
-        .catch(console.error);
+  // ACTION: recover the whole book list
+  app.get('/books', async (req, res) => {
+    try {
+      const books = await Book.find({}, null, null).exec();
+      if (!books.length) return res.status(204).json();
+      res.json(books);
+    } catch (e) {
+      res.status(500).json(e);
+    }
   });
+  // ACTION: recover a book detail (filtered by _id)
+  app.get('/books/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const book = await Book.findById(id).exec();
+      if (!book) return res.status(404).json();
+      res.json(book);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  })
+  // ACTION: add a new book to the list
+  app.post('/books', authRequired, fileInterceptor, async (req, res) => {
+    const userId = req.userId;
+    const { title, author, genre, imageUrl, year } = req.body;
+
+    try {
+      const data = await Book.create({
+        userId,
+        title,
+        author,
+        genre,
+        imageUrl,
+        year,
+      });
+      res.status(201).json(data);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  })
 };

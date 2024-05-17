b37e7972eee251de7884adb5831039ffe6fee12d
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-05-17 14:55:42 +0200

    :rocket: Start file middleware logic

--- src/middlewares/file.middleware.js
+++ src/middlewares/file.middleware.js
@@ -1,4 +1,6 @@
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   // TODO: save file and update field to an uri
+  if (req.body.image) delete req.body.image;
+  req.body.imageUrl = 'https://www.maesloic.fr/public/assets/images/unsplash.com.jpeg';
   next();
 }

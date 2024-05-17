791fea8d222e58055dcaf809f70ab11112623213
loicmaes <production@maesloic.fr>
2024-05-17 16:27:55 +0200

    :rocket: Handle file compression and save it to /images directory

--- src/middlewares/file.middleware.js
+++ src/middlewares/file.middleware.js
@@ -1,6 +1,31 @@
+const sharp = require('sharp');
+
 module.exports = async (req, res, next) => {
-  // TODO: save file and update field to an uri
-  if (req.body.image) delete req.body.image;
-  req.body.imageUrl = 'https://www.maesloic.fr/public/assets/images/unsplash.com.jpeg';
-  next();
+  try {
+    if (!req.file) next();
+
+    const { buffer, originalname } = req.file;
+    const filename = originalname.split(' ').join('_');
+    const filenameArray = filename.split('.');
+    filenameArray.pop();
+    const filenameWithoutExtension = filenameArray.join('.');
+    const filePath = `${filenameWithoutExtension}_${Date.now()}.webp`;
+
+    const path = `images/${filePath}`;
+
+    await sharp(buffer)
+        .resize(1024)
+        .webp({
+          quality: 90,
+        })
+        .toFile(`./${path}`);
+    req.body.imageUrl = `${req.protocol}://${req.get('host')}/${path}` ?? undefined;
+
+    next();
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Something went wrong!',
+      error: e,
+    });
+  }
 }
